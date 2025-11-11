59e571ecb3933410fc1935ed6433caf017fffc5f
Valer BOCAN <vbocan@gmail.com>
2025-11-11 06:35:17 +0200

    Use QRNG_ prefix for environment config loading

--- qrng-core/src/config.rs
+++ qrng-core/src/config.rs
@@ -90,7 +90,8 @@ impl CollectorConfig {
 impl CollectorConfig {
     /// Load configuration from environment variables
     pub fn from_env() -> Result<Self> {
-        let config: Self = envy::from_env()
+        let config: Self = envy::prefixed("QRNG_")
+            .from_env()
             .map_err(|e| Error::Config(format!("Failed to parse environment variables: {}", e)))?;
         config.validate()?;
         Ok(config)
